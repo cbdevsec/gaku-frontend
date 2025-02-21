ac02cb92d51247e5153cb65ac81946566b51dd21
CBDEVSEC <ayakhiouat@gmail.com>
2025-02-21 17:24:04 +0100

    Update streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -10,7 +10,7 @@ import os
 
 
 # 🔹 Configure API URL
-API_URL = "http://127.0.0.1:5000"
+API_URL = "https://gaku-backend.onrender.com"
 
 # 🔹 Force NLTK to use a local directory
 nltk.data.path.append(os.path.join(os.path.dirname(__file__), "nltk_data"))
